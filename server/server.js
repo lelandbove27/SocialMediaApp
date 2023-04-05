a4e76a17fc79f2102607edea61857292e92b0b92
lbove27 <lbove@ucsd.edu>
2023-04-05 14:15:32 -0700

    first commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const Joi = require('joi');
+//const Joi = require('joi');
 const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.options('*', cors());
+
+//let jsonParser = bodyParser.json();
 
 app.get('/images', (req, res) => {
     res.send("hi");
@@ -12,11 +18,10 @@ app.get('/images', (req, res) => {
 
 app.post('/uploadImage', (req, res) => {
     const { image } = req.files;
-
     if(!image) return res.sendStatus(400);
     image.mv('/Users/lelan/OneDrive/Desktop/Meme-App/server' + '/uploadImage/' + image.name);
-
-    res.sendStatus(200);
+    res.set('Access-Control-Allow-Origin', '*');
+    return res.sendStatus(200);
 });
 
 const port = process.env.PORT || 8080;
